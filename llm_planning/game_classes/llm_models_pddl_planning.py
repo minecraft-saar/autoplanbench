86f6499915b44551346f857cda704083d18d8e3e
kastein <kstein@coli.uni-saarland.de>
2023-11-20 18:16:58 +0100

    pddl game classes

---
+++ llm_planning/game_classes/llm_models_pddl_planning.py
@@ -0,0 +1,45 @@
+from typing import List, Tuple, Union
+from model_classes.planning_game_models import TranslationModel, PlanningModel
+
+class PlanningModelBlocksWorld(PlanningModel):
+
+    def __init__(self, model_type: str, model_param: dict, example_dict: dict, init_prompt: str):
+        """
+
+        :param model_type:
+        :param model_param:
+        :param example_dict:
+        :param init_prompt:
+        """
+        self.model_param = model_param
+        super().__init__(model_type=model_type, model_param=model_param, examples_dict=example_dict, init_prompt=init_prompt)
+
+
+    def generate(self, user_message):
+        assert type(user_message) == str
+        if not self.examples_chat:
+            user_message = self.create_input_format_example(user_message)
+        output = self.model.generate(user_message)
+        return output
+
+
+
+
+class TranslationModelBlocksWorld(TranslationModel):
+
+    def __init__(self, model_type: str, model_param: dict, example_dict: dict, init_prompt: str):
+        """
+
+        :param model_type:
+        :param model_param:
+        :param example_dict:
+        :param init_prompt:
+        """
+        super().__init__(model_type=model_type,
+                         model_param=model_param,
+                         examples_dict=example_dict,
+                         init_prompt=init_prompt)
+
+
+
+
